48add589a4921564efe9741609abaed2514999f3
andre-alck <andrealck1@gmail.com>
2022-08-23 14:07:55 -0300

    update properties

--- src/main/java/com/pxp/employeedatabase/controller/EmployeeController.java
+++ src/main/java/com/pxp/employeedatabase/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.pxp.employeedatabase.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import com.pxp.employeedatabase.model.Employee;
 import com.pxp.employeedatabase.service.EmployeeService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
